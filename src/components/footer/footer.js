610e8edede18a4fbc82a50d1c424f5587265ff53
Ivo Meißner <info@overtronic.com>
2020-10-05 16:47:03 -0500

    Teaser, footer styling, slicknode quicklinks

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,21 @@ import styles from './footer.module.scss';
 export const Footer = (props) => {
   return (
     <footer className={styles.footer}>
-      Footer
+      Slicknode Quicklinks:
+      <ul className={styles.menu}>
+        <li>
+          <a href="https://slicknode.com/docs/" target="_blank">Docs</a>
+        </li>
+        <li>
+          <a href="https://console.slicknode.com/" target="_blank">Admin</a>
+        </li>
+        <li>
+          <a href="https://slicknode.com/" target="_blank">Website</a>
+        </li>
+        <li>
+          <a href="https://slicknode.com/slack" target="_blank">Slack Channel</a>
+        </li>
+      </ul>
     </footer>
   );
 };
